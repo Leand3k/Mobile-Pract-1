2ddf094d0cf0218050697950f25d4112e96c72ae
Leand3000 <theleand3000@gmail.com>
2021-08-19 02:19:54 -0400

    Images added to drawable. Working on contentpage.

--- Practica-1/Practica-1.Android/obj/Debug/110/android/environment.x86_64.s
+++ Practica-1/Practica-1.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"1fc9ccf4-f24a-4c37-b698-0bba9fe5a84e"
+	.asciz	"58db4e67-4fe8-41b3-9189-adebe77c4d01"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
